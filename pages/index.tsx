c0c0638c7f76b6cda38aecd47bdb5835bd9b023a
Alexei Samuta <alexeisamuta@gmail.com>
2021-08-19 19:51:54 +0300

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.hello}>
+            Hello world
+        </div>
+    )
+}
+
+export default Home
